1a6c77ca18fb96235846d70c19727ac711b07039
Tomer Klein <tomer.klein@gmail.com>
2020-09-09 00:26:50 +0300

    Update dockerbot.py

--- dockerbot.py
+++ dockerbot.py
@@ -1,77 +1,49 @@
-# -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from datetime import date
-from datetime import time
-from datetime import datetime
-import time
-from argparse import ArgumentParser
-import os
-from selenium.common.exceptions import InvalidSessionIdException
+import time, re, random, datetime, telepot
+from subprocess import call
+import subprocess, os, sys
+from telepot.loop import MessageLoop
+from flask import Flask, request, make_response, render_template, url_for, g, send_file
+from flask import send_from_directory, jsonify
+from flask_restful import Resource, Api
 
-parser = ArgumentParser()
-parser.add_argument("-u", "--user", dest="userCode")
-parser.add_argument("-p", "--pass", dest="SitePassword")
-parser.add_argument("-k", "--kid", dest="KidCovid")
+# #Vars for Selenium covid kids approval
+# v_UserId = os.getenv('USER_ID')
+# v_UserKey = os.getenv('USER_KEY') 
+# v_KidTotal = os.getenv('KIDS_NUM')
+# #Auto Commmand List
 
 
-args = parser.parse_args()
-userCode = args.userCode
-SitePassword = args.SitePassword
-KidCovid = args.KidCovid
-option = webdriver.ChromeOptions()
-option.add_argument("-incognito")
-option.add_argument("--headless")
-option.add_argument("disable-gpu")
-option.add_argument("--no-sandbox")
-option.add_argument('--start-maximized')
-option.add_argument("--disable-dev-shm-usage")
-option.add_argument("--window-size=800,600")
+app = Flask(__name__)
+api = Api(app)
+app.config['v_UserId']=os.getenv('USER_ID')
+app.config['v_UserKey']=os.getenv('USER_KEY')
+app.config['v_KidTotal']=os.getenv('KIDS_NUM')
 
 
-def fullpage_screenshot():
-    print(browser)
-    browser.set_window_size(800, 600) #the trick
-    time.sleep(2)
-    image = "/opt/Approval_form.png"
-    browser.save_screenshot(image)
-    browser.close()
+@app.route('/sign')
+def sign():
+    try:
+        for file in os.listdir(os.path.join(app.root_path,'..')):
+            if file.endswith(".png"):
+                Image = os.path.join(app.root_path,'..', file)
+                os.remove(str(Image))
+        subprocess.check_output('/usr/bin/python ' + os.path.join(app.root_path,'Health_Staytments.py') + ' -u ' + app.config['v_UserId'] +  ' -p ' + app.config['v_UserKey'] + ' -k sign'  , shell=True)
+        return jsonify('{"success":"1","data":""}')
+        # bot.sendPhoto(chat_id=chat_id, photo=open(str(Image), 'rb'))
+    except Exception as e:
+        return jsonify('{"success":"1","data":"' + str(e) + '"}')
 
 
-print("Strating process")
+@app.route('/statement')
+def getStatement():
+    for file in os.listdir(os.path.join(app.root_path,'..')):
+        if file.endswith(".png"):
+            Image = os.path.join(app.root_path,'..', file)
+            return send_file(str(Image), mimetype='image/png')
+    Image = os.path.join(app.root_path, 'please_sign.jpg')
+    return send_file(str(Image), mimetype='image/jpeg')
 
-browser = webdriver.Chrome(executable_path="/opt/chromedriver-85.0.4183.87/chromedriver", options=option)
-browser.get("https://parents.education.gov.il/prhnet/parents/rights-obligations-regulations/health-statement-kindergarden")
-start = '//*[@id="main-content"]/section[1]/div/health-declaration/div/div[1]/div[4]/div/div/div/input'
-time.sleep( 2 )
-browser.find_element_by_xpath(start).click()
-time.sleep( 2 )
-user = '//*[@id="HIN_USERID"]'
-siteAccess = '//*[@id="Ecom_Password"]'
-NextPhase = '//*[@id="loginButton2"]'
 
-browser.find_element_by_xpath(user).send_keys(userCode)
-browser.find_element_by_xpath(siteAccess).send_keys(SitePassword)
-browser.find_element_by_xpath(NextPhase).click()
-time.sleep( 2 )
-print("Logged IN...")
-element = "//input[@value='מילוי הצהרת בריאות']"
-checkForButton = browser.find_elements_by_xpath(element)
-LenCheckForButton = len(checkForButton)
-if KidCovid == 'sign':
-    print("starting Sign..")
-    print(len(checkForButton))
-    if int(LenCheckForButton) == 0:
-        fullpage_screenshot()
-    elif LenCheckForButton > 0:
-        for x in range(LenCheckForButton):
-            print(x + 1) 
-            browser.find_element_by_xpath(element).click()
-            time.sleep( 2 )
-            ToApprove = browser.find_elements_by_xpath("//input[@value='אישור']")
-            for a in ToApprove:
-                browser.execute_script("arguments[0].click()", a)
-            time.sleep( 2 )
-        fullpage_screenshot()
-    else:
-        print("Used else...")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=6700)
